f2060f2f6657da88e922605ee2c7c150b2f95314
Valérian Galliat <val@codejam.info>
2015-03-18 10:02:14 +0100

    Remove semicolon

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ import done from './'
 import Promise from 'promise'
 
 const expectedError = new Error()
-let foundError;
+let foundError
 
 process.on('uncaughtException', err => foundError = err)
 Promise.reject(expectedError).then(null, done)
